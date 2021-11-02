ab67c4ea992b80ee0a24cfbdd04a969f2d9c8654
qin040808 <qin0808@naver.com>
2021-11-02 11:32:53 +0900

    socket connection set

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { join } from "path";
+import socketIo from "socket.io";
+import logger from "morgan";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.get("/",(req, res) => res.render("home"));
+app.use(express.static(join(__dirname, "static")));
+
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server Running : http://localhost:${PORT}`);
+})
+const io = socketIo(server);
+
+io.on("connection", () => console.log("somebody connect"));
