8571b5bc7ca318e759fb54a7c7591ae8003081ce
qin040808 <qin0808@naver.com>
2021-11-04 09:54:39 +0900

    css improvements
    gulp minimize and auto prefixer

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp from "gulp";
 import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
+import autoPrefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
 
 const sass = gulpSass(nodeSass);
 
@@ -8,5 +10,9 @@ export function styles() {
     return gulp
         .src("assets/scss/styles.scss")
         .pipe(sass())
+        .pipe(autoPrefixer({
+            cascade: false,
+        }))
+        .pipe(minifyCSS())
         .pipe(gulp.dest("src/static/styles"));
 }
